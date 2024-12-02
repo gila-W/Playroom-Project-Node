d80d3e53dc4ff61387d9e62ca6bbe3493507731d
yafa <yafa0694@gmail.com>
2024-12-02 11:55:33 +0200

    commit

--- Models/users.js
+++ Models/users.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 usersschema = new mongoose.Schema({
   userCode: String,
   userName: String,
-  cellphone: String,
+  userDate: String,
   phone: String,
+  cellphone: String,
+  depositPaid: String,
+  paymentType: String,
+  totalPayment: String,
+  bankNumber: String,
+  accountNumber: String,
+  checkNumber: String,
+  branchNumber: String,
+  email: String,
 });
 exports.UserModel = mongoose.model("Users", usersschema);
