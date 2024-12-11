13be9201d4dcbfafa98df42187ca46f10fdd795c
yafa <yafa0694@gmail.com>
2024-12-11 12:18:47 +0200

    מחיקת האופציות של ההרשאות

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-"use strict";
-const myExpress = require("express");
+"use strict"
+const myExpress = require('express');
 const app = myExpress();
 const port = 5000;
-const router = myExpress.Router();
-const http = require("http");
-require("./DB/mongoConnection");
-const { routesInit } = require("./Routes/configRoutes");
-const path = require("path");
-const cors = require("cors");
-app.use(cors());
-const bodyParser = require("body-parser");
+const router=myExpress.Router();
+const http=require('http')
+require('./DB/mongoConnection');
+const{routesInit}=require("./Routes/configRoutes");
+const path=require('path');
+const cors=require("cors");
+const bodyParser=require("body-parser");
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000" }));
-app.use(myExpress.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
+app.use(cors());
+app.use(myExpress.static(path.join(__dirname,"public")));
 routesInit(app);
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-const server = http.createServer(app);
-app.get("/", (req, res) => {
-});
+    console.log(`Server running at http://localhost:${port}`);
+  });
+const server=http.createServer(app);
+app.get("/",(req,res)=>{
+    // res.send("<html><h1>מחובר</h1></html>")
+        })
